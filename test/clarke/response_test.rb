14abab0a9234028a48895f2e1b5e9b9e45d40fc3
Hugo Hache <hugo.hache@fabernovel.com>
2017-11-09 11:47:34 +0100

    Response JSON serialization (useful if you want to delay the deliver process)

--- test/clarke/response_test.rb
+++ test/clarke/response_test.rb
@@ -1,19 +1,28 @@
-require "./test/test_helper"
+require './test/test_helper'
 
 class TestClarkeResponse < Test::Unit::TestCase
-
   setup do
   end
 
-  test "create an empty response" do
+  test 'create an empty response' do
     response = Clarke::Response.new('abc123')
     assert_equal('abc123', response.recipient)
   end
 
-  test "create a response with text" do
+  test 'create a response with text' do
     response = Clarke::Response.new('abc123', {text: 'hello'})
     assert_equal('abc123', response.recipient)
     assert_equal('hello', response.text)
   end
 
+  test 'to json' do
+    response = Clarke::Response.new('abc123', {text: 'hello'})
+    assert_equal('{"recipient":"abc123","options":{"text":"hello"}}', response.to_json)
+  end
+
+  test 'from json' do
+    response = Clarke::Response.from_json('{"recipient":"abc123","options":{"text":"hello"}}')
+    assert_equal 'abc123', response.recipient
+    assert_equal 'hello', response.options[:text]
+  end
 end
